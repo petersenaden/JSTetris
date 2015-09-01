da4033f83b3c0bd50c05e6383e856ef9b3a51ec5
petersenaden <petersen.aden@gmail.com>
2015-08-31 17:11:59 -0700

    Now it works

--- shapes.js
+++ shapes.js
@@ -1,9 +1,11 @@
 
 
-function drawRect() {
-var c=document.getElementById("tetrisBoard");
-var ctx=c.getContext("2d");
-ctx.rect(0,0,150,150);
-ctx.stroke();
+function drawRect(xcoord, ycoord, width, height)
+{
+	var c=document.getElementById("tetrisBoard");
+	var ctx=c.getContext("2d");
+	ctx.rect(xcoord, ycoord, width, height);
+	ctx.stroke();
 }
-drawRect()
+
+drawRect(0,0,100,100);
