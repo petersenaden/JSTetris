9d1545925e8d038b9530e806c90b71db0567d7ac
petersenaden <petersen.aden@gmail.com>
2015-08-31 17:20:25 -0700

    Forgot to call function

--- shapes.js
+++ shapes.js
@@ -1,14 +1,5 @@
 
 
-function drawSquare(xcoord, ycoord, width, height)
-{
-	drawBlock(200, 0, 100, 100);
-	drawBlock(300, 0, 100, 100);
-	drawBlock(200, 100, 100, 100);
-	drawBlock(300, 100, 100, 100);
-}
-
-
 
 function drawBlock(xcoord, ycoord, width, height)
 {
@@ -17,3 +8,15 @@ function drawBlock(xcoord, ycoord, width, height)
 	ctx.rect(xcoord, ycoord, width, height);
 	ctx.stroke(); //Actually draws what has been defined
 }
+
+
+
+function drawSquare()
+{
+	drawBlock(200, 0, 100, 100);
+	drawBlock(300, 0, 100, 100);
+	drawBlock(200, 100, 100, 100);
+	drawBlock(300, 100, 100, 100);
+}
+
+drawSquare()
