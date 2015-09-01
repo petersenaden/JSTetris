08b2d28c70f3df9e384f464ca2852b58c6f529f2
petersenaden <petersen.aden@gmail.com>
2015-08-31 17:19:13 -0700

    Added square logic

--- shapes.js
+++ shapes.js
@@ -1,11 +1,21 @@
 
 
-function drawRect(xcoord, ycoord, width, height)
+function drawSquare(xcoord, ycoord, width, height)
+{
+	drawBlock(200, 0, 100, 100);
+	drawBlock(300, 0, 100, 100);
+	drawBlock(200, 100, 100, 100);
+	drawBlock(300, 100, 100, 100);
+}
+
+
+
+function drawBlock(xcoord, ycoord, width, height)
 {
 	var c=document.getElementById("tetrisBoard");
 	var ctx=c.getContext("2d");
 	ctx.rect(xcoord, ycoord, width, height);
-	ctx.stroke();
+	ctx.stroke(); //Actually draws what has been defined
 }
 
-drawRect(0,0,100,100);
+drawBlock(0,0,100,100);
