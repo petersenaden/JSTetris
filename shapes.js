c72cfa15a1c869b5c843757804ee506d7cc39770
petersenaden <petersen.aden@gmail.com>
2015-09-01 17:31:14 -0700

    Testing drawn square

--- shapes.js
+++ shapes.js
@@ -1,5 +1,6 @@
 //GLOBALS
 var tetrisGrid;
+var boardName = "tetrisBoard"
 //GLOBALS
 
 function sqr() {
@@ -7,6 +8,14 @@ function sqr() {
 	this.topr = [0,0];
 	this.btml = [0,0];
 	this.btmr = [0,0];
+	this.drawSquareTop = function() {
+		var canvas = document.getElementById(boardName);
+		var width = canvas.width / 2 - ((canvas.width / 2) % 100);
+		drawBlock(width, 0);
+		drawBlock(width + 100, 0);
+		drawBlock(width, 100);
+		drawBlock(width + 100, 100);
+	}
 }
 
 function leftl() {
@@ -14,6 +23,14 @@ function leftl() {
 	this.mid = [0,0];
 	this.bot = [0,0];
 	this.tail = [0,0];
+	this.drawLeftLTop = function() {
+		var canvas = document.getElementById(boardName);
+		var width = canvas.width / 2 - ((canvas.width / 2) % 100);
+		drawBlock(width, 0);
+		drawBlock(width + 100, 0);
+		drawBlock(width + 200, 0);
+		drawBlock(width + 200, 100);
+	}
 }
 
 function rightl() {
@@ -21,24 +38,32 @@ function rightl() {
 	this.mid = [0,0];
 	this.bot = [0,0];
 	this.tail = [0,0];
+	this.drawRightLTop = function() {
+		var canvas = document.getElementById(boardName);
+		var width = canvas.width / 2 - ((canvas.width / 2) % 100);
+		drawBlock(width, 0);
+		drawBlock(width + 100, 0);
+		drawBlock(width + 200, 0);
+		drawBlock(width, 100);
+	}
 }
 
-function ActivePiece() {
+function ActivePiece(type) {
     this.type = type;
-    if (this.type === "sqr") {
+    if (this.type === 0) {
     	activePiece = sqr();
     }
-    else if (this.type === "leftl") {
+    else if (this.type === 1) {
     	activePiece = leftl();
     }
-    else if (this.type === "rightl") {
+    else if (this.type === 2) {
     	activePiece = rightl();
     }
 }
 
 function createTetrisGrid() {
 //0 will correspond to empty, else will be full
-var canvas = document.getElementById("tetrisBoard");
+var canvas = document.getElementById(boardName);
 var width = canvas.width / 100;
 var height = canvas.height / 100;
 tetrisGrid = createMatrix(width, height, 0);
@@ -59,37 +84,16 @@ function createMatrix( rows, cols, defaultValue)
     }
 }
 
-
-function drawBlock(xcoord, ycoord, width, height)
+function drawBlock(xcoord, ycoord)
 {
-	var c=document.getElementById("tetrisBoard");
+	var blockWidth = 100;
+	var blockHeight = 100;
+	var c=document.getElementById(boardName);
 	var ctx=c.getContext("2d");
-	ctx.rect(xcoord, ycoord, width, height);
+	ctx.rect(xcoord, ycoord, blockWidth, blockHeight);
 	ctx.stroke(); //Actually draws what has been defined
 }
 
-function drawSquare()
-{
-	drawBlock(200, 0, 100, 100);
-	drawBlock(300, 0, 100, 100);
-	drawBlock(200, 100, 100, 100);
-	drawBlock(300, 100, 100, 100);
-}
-
-function drawLeftL()
-{
-	drawBlock(100, 0, 100, 100);
-	drawBlock(200, 0, 100, 100);
-	drawBlock(300, 0, 100, 100);
-	drawBlock(300, 100, 100, 100);
-}
-
-function drawRightL()
-{
-	drawBlock(100, 0, 100, 100);
-	drawBlock(200, 0, 100, 100);
-	drawBlock(300, 0, 100, 100);
-	drawBlock(100, 100, 100, 100);
-}
-
-drawRightL()
+createTetrisGrid();
+activePiece1 = activePiece(0);
+activePiece1.drawSquareTop();
