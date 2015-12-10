3bd8ba56f356f6311baef2bb8997a38dd99435d1
petersenaden <petersen.aden@gmail.com>
2015-12-10 15:01:55 -0800

    Made changes more visible

--- main.js
+++ main.js
@@ -5,5 +5,8 @@ mainGame.executeDropPiece();
 mainGame.executeRotatePiece(0);
 mainGame.executeDropPiece();
 mainGame.executeDropPiece();
+mainGame.executeDropPiece();
+mainGame.executeDropPiece();
 mainGame.createPiece(0);
+mainGame.executeDropPiece();
 mainGame.executeDropPiece();
