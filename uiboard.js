34731d1725606d865f5bd1e9d0b80189a47b4e6e
petersenaden <petersen.aden@gmail.com>
2015-12-20 01:19:42 -0800

    Fixed Updatescore and added line for strucutre

--- uiboard.js
+++ uiboard.js
@@ -3,6 +3,7 @@ function updateAllMetrics(currScore, currLines, currLevel) {
 	updateLevel(currLevel);
 	updateLines(currLines);
 	updateScore(currScore);
+	drawMetricOutline();
 }
 
 function clearScoreBoard() {
@@ -14,24 +15,30 @@ function clearScoreBoard() {
 
 function updateLevel(currLevel) {
 	//ERROR CHECK THESE
-	var canvas = document.getElementById("scoreBoard");
-	var ctx = canvas.getContext("2d");
-	ctx.font = "30px Impact";
-	ctx.fillText("Level: " + currLevel, 10, 180);
+	var scoreCanvas = document.getElementById("scoreBoard");
+	var scoreContext = scoreCanvas.getContext("2d");
+	scoreContext.font = "30px Impact";
+	scoreContext.fillText("Level: " + currLevel, 10, 180);
 }
 
 function updateLines(currLines) {
 	//THESE NEED TO BE ERROR CHECKED
-	var canvas = document.getElementById("scoreBoard");
-	var ctx = canvas.getContext("2d");
-	ctx.font = "30px Impact";
-	ctx.fillText("Lines: " + currLines, 10, 240);
+	var scoreCanvas = document.getElementById("scoreBoard");
+	var scoreContext = scoreCanvas.getContext("2d");
+	scoreContext.font = "30px Impact";
+	scoreContext.fillText("Lines: " + currLines, 10, 240);
 }
 
 function updateScore(currScore) {
 	//ERROR CHECK THESE
-	var canvas = document.getElementById("scoreBoard");
-	var ctx = canvas.getContext("2d");
-	ctx.font = "30px Impact";
-	ctx.fillText("Score: " + currScore, 10, 300);
+	var scoreCanvas = document.getElementById("scoreBoard");
+	var scoreContext = scoreCanvas.getContext("2d");
+	scoreContext.font = "30px Impact";
+	scoreContext.fillText("Score: " + currScore, 10, 300);
+}
+
+function drawMetricOutline() {
+	var scoreCanvas = document.getElementById("scoreBoard");
+	var scoreContext = scoreCanvas.getContext("2d");
+	scoreContext.fillRect(0,150,5,170);
 }
